9d8ab78abfe003cc5f4511275a551e54e38ac454
Rahul Mahesh <41413029+RahulMahesh62@users.noreply.github.com>
2020-12-08 23:15:27 +0530

    Added API

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-let jokeContent = document.querySelector("");
+let jokeContent = document.querySelector("#quote");
+let nextButton = document.querySelector("#next");
+document.addEventListener('DOMContentLoaded',NextQuote);
+
+async function NextQuote() {
+	const url = await fetch("https://icanhazdadjoke.com/", {
+		headers: {
+			'Accept': 'application/json'
+		}
+	}); 
+	const obj = await url.json();
+	jokeContent.innerHTML = obj.joke;
+}
+
+nextButton.addEventListener('click', NextQuote);
